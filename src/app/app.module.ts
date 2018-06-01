56d48f6aca91d15976ba2c26c8fe0e8320f51438
Shuhao Chen <shuhao.chen@sjsu.edu>
2018-06-01 11:55:53 -0700

    authLogin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,16 @@ import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
 
 
 const addRoutes: Routes = [
     { path: '' ,component: HomeComponent },
-    { path: 'users' ,component: UsersComponent },
-    { path: 'users/:id/:name' ,component: UsersComponent },
-    { path: 'servers' ,component: ServersComponent ,children:[
+    { path: 'users' ,component: UsersComponent , children:[
+        { path: ':id/:name' ,component: UsersComponent }
+    ]},
+    { path: 'servers' , canActivateChild: [AuthGuard],  component: ServersComponent ,children:[
         { path: ':id' ,component: ServerComponent },
         { path: ':id/edit' ,component: EditServerComponent }
     ]},
@@ -44,7 +47,7 @@ const addRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(addRoutes)
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
